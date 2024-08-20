920f9360b475e4f6e0f2b9f8c5a1aa49506e6793
Ryan McDaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-08-20 03:50:04 -0500

    chore: esbuild cleanup (#4)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,6 +5,7 @@ variable "reg" {}
 variable "service" {}
 
 variable "coc_url" {}
-variable "coc_ssm_token" {}
+variable "coc_ssm_user" {}
+variable "coc_ssm_password" {}
 variable "discord_url" {}
 variable "discord_ssm_token" {}
