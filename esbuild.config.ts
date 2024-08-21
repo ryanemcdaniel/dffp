f4597c5a8adddcbbb230a39a2cc4c9a5c651b469
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-08-20 23:43:13 -0500

    feat: api gateway connected

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -3,20 +3,25 @@ import {readdir} from 'node:fs/promises';
 import {resolve} from 'node:path';
 
 export default {
+    entryPoints:
+        (await readdir('src', {withFileTypes: true, recursive: true}))
+            .filter((l) => l.name === 'index.ts')
+            .map((l) => resolve(l.path, l.name)),
 
-    outdir        : 'dist',
-    allowOverwrite: true,
-    entryPoints   : (await readdir('src', {withFileTypes: true, recursive: true}))
-        .filter((l) => l.name === 'index.ts')
-        .map((l) => resolve(l.path, l.name)),
+    platform    : 'node',
+    target      : 'node20',
+    format      : 'esm',
+    outExtension: {'.js': '.mjs'},
 
-    platform      : 'node',
-    target        : 'node20',
-    format        : 'esm',
-    bundle        : true,
+    bundle           : true,
+    minifySyntax     : true,
+    minifyWhitespace : true,
+    minifyIdentifiers: false,
+
     sourcemap     : 'linked',
     sourcesContent: false,
+    outdir        : 'dist',
 
-    logLevel: 'info',
-
+    logLevel      : 'info',
+    allowOverwrite: true,
 } satisfies BuildOptions;
