f1d35683b3d847645ce445392022fbf664605f7f
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-08-20 23:44:38 -0500

    feat: api gateway connected

--- terraform/modules/lambda/cloudwatch.tf
+++ terraform/modules/lambda/cloudwatch.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "logs" {
-  name              = "/aws/lambda/${var.prefix}-${var.fn_name}"
+  name              = "/lambda/${var.prefix}-${var.fn_name}"
   retention_in_days = 7
 }
