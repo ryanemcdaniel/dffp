299a687798cad0313719e6cecfe5f3a8694a84f7
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-08-20 20:30:44 -0500

    refactor: rename lambdas

--- terraform/api.tf
+++ terraform/api.tf
@@ -1,62 +1,62 @@
-resource "aws_api_gateway_rest_api" "api_discord" {
-    name = "${local.prefix}-api-discord"
-    endpoint_configuration {
-        types = ["REGIONAL"]
-    }
-}
-
-resource "aws_api_gateway_resource" "api_discord" {
-    rest_api_id = aws_api_gateway_rest_api.api_discord.id
-    parent_id   = aws_api_gateway_rest_api.api_discord.root_resource_id
-    path_part   = "dffp"
-}
-
-resource "aws_api_gateway_method" "api_discord" {
-    rest_api_id        = aws_api_gateway_rest_api.api_discord.id
-    resource_id        = aws_api_gateway_resource.api_discord.id
-    http_method        = "POST"
-    authorization      = "NONE"
-}
-
-resource "aws_api_gateway_method_response" "proxy" {
-    rest_api_id = aws_api_gateway_rest_api.api_discord.id
-    resource_id = aws_api_gateway_resource.api_discord.id
-    http_method = aws_api_gateway_method.api_discord.http_method
-    status_code = "200"
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = true,
-        "method.response.header.Access-Control-Allow-Methods" = true,
-        "method.response.header.Access-Control-Allow-Origin" = true
-    }
-}
-
-resource "aws_api_gateway_integration" "api_discord_post" {
-    rest_api_id             = aws_api_gateway_rest_api.api_discord.id
-    resource_id             = aws_api_gateway_resource.api_discord.id
-    http_method             = aws_api_gateway_method.api_discord.http_method
-    integration_http_method = "POST"
-    type                    = "AWS_PROXY"
-    uri                     = module.lambda_api_discord.fn_invoke_arn
-}
-
-resource "aws_api_gateway_integration_response" "proxy" {
-    rest_api_id         = aws_api_gateway_rest_api.api_discord.id
-    resource_id         = aws_api_gateway_resource.api_discord.id
-    http_method         = aws_api_gateway_method.api_discord.http_method
-    status_code         = aws_api_gateway_method_response.proxy.status_code
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-        "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-    }
-}
-
-resource "aws_api_gateway_deployment" "api_discord" {
-    rest_api_id = aws_api_gateway_rest_api.api_discord.id
-    triggers    = {
-        redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api_discord.body))
-    }
-    lifecycle {
-        create_before_destroy = true
-    }
-}
+# resource "aws_api_gateway_rest_api" "api_discord" {
+#     name = "${local.prefix}-api-discord"
+#     endpoint_configuration {
+#         types = ["REGIONAL"]
+#     }
+# }
+#
+# resource "aws_api_gateway_resource" "api_discord" {
+#     rest_api_id = aws_api_gateway_rest_api.api_discord.id
+#     parent_id   = aws_api_gateway_rest_api.api_discord.root_resource_id
+#     path_part   = "dffp"
+# }
+#
+# resource "aws_api_gateway_method" "api_discord" {
+#     rest_api_id        = aws_api_gateway_rest_api.api_discord.id
+#     resource_id        = aws_api_gateway_resource.api_discord.id
+#     http_method        = "POST"
+#     authorization      = "NONE"
+# }
+#
+# resource "aws_api_gateway_method_response" "proxy" {
+#     rest_api_id = aws_api_gateway_rest_api.api_discord.id
+#     resource_id = aws_api_gateway_resource.api_discord.id
+#     http_method = aws_api_gateway_method.api_discord.http_method
+#     status_code = "200"
+#     response_parameters = {
+#         "method.response.header.Access-Control-Allow-Headers" = true,
+#         "method.response.header.Access-Control-Allow-Methods" = true,
+#         "method.response.header.Access-Control-Allow-Origin" = true
+#     }
+# }
+#
+# resource "aws_api_gateway_integration" "api_discord_post" {
+#     rest_api_id             = aws_api_gateway_rest_api.api_discord.id
+#     resource_id             = aws_api_gateway_resource.api_discord.id
+#     http_method             = aws_api_gateway_method.api_discord.http_method
+#     integration_http_method = "POST"
+#     type                    = "AWS_PROXY"
+#     uri                     = module.lambda_api_discord.fn_invoke_arn
+# }
+#
+# resource "aws_api_gateway_integration_response" "proxy" {
+#     rest_api_id         = aws_api_gateway_rest_api.api_discord.id
+#     resource_id         = aws_api_gateway_resource.api_discord.id
+#     http_method         = aws_api_gateway_method.api_discord.http_method
+#     status_code         = aws_api_gateway_method_response.proxy.status_code
+#     response_parameters = {
+#         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+#         "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+#         "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+#     }
+# }
+#
+# resource "aws_api_gateway_deployment" "api_discord" {
+#     rest_api_id = aws_api_gateway_rest_api.api_discord.id
+#     triggers    = {
+#         redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api_discord.body))
+#     }
+#     lifecycle {
+#         create_before_destroy = true
+#     }
+# }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -26,25 +26,25 @@ data "aws_iam_policy_document" "lambda_api_discord" {
   }
 }
 
-resource "aws_lambda_permission" "api_discord" {
-  function_name = module.lambda_api_discord.fn_name
-  action        = "lambda:InvokeFunction"
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api_discord.id}/*/${aws_api_gateway_method.aws_api_gw_method.http_method}${aws_api_gateway_resource.api_discord.path}"
-}
+# resource "aws_lambda_permission" "api_discord" {
+#   function_name = module.lambda_api_discord.fn_name
+#   action        = "lambda:InvokeFunction"
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn    = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api_discord.id}/*/${aws_api_gateway_method.api_discord.http_method}${aws_api_gateway_resource.api_discord.path}"
+# }
 
 
-module "lambda_poll" {
+module "lambda_poll_coc" {
   source      = "./modules/lambda"
   prefix      = local.prefix
   fn_name     = "poll_coc"
-  policy_json = data.aws_iam_policy_document.lambda_poll.json
+  policy_json = data.aws_iam_policy_document.lambda_poll_coc.json
   memory      = 128
   timeout     = 300
   fn_env      = local.lambda_env
 }
 
-data "aws_iam_policy_document" "lambda_poll" {
+data "aws_iam_policy_document" "lambda_poll_coc" {
   statement {
     effect = "Allow"
     actions = ["*"]
