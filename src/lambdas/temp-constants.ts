7254800bf3075db5ca3001896d61d21f34b2e713
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-09-11 21:10:19 -0500

    chore: add aliases

--- src/lambdas/temp-constants.ts
+++ src/lambdas/temp-constants.ts
@@ -13,11 +13,14 @@ export const DFFP_CLANS_ALIAS = {
     main          : '#2GR2G0PGG',
     labs          : '#2GYYRUULL',
     dffplabs      : '#2GYYRUULL',
+    ct            : '#2YVCYUCCP',
     ctd           : '#2YVCYUCCP',
     ctdummy       : '#2YVCYUCCP',
     dummy         : '#2YVCYUCCP',
     clashtestdummy: '#2YVCYUCCP',
     ezcwl         : '#2QUP9UPGY',
+    ace           : '#2P8UCYQQ0',
+    acemini       : '#2R8L9QQG2',
 };
 
 export const DFFP_PLAYERS = [
